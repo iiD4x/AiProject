cc437ed9cc3659cd7b1b4670f5914137505f268c
abodamin <abodamin0@gmail.com>
2018-04-18 02:39:46 +0300

    Sync Projects

--- src/ai/project/Population.java
+++ src/ai/project/Population.java
@@ -3,7 +3,20 @@ package ai.project;
 
 import java.util.ArrayList;
 
+import static java.lang.Integer.MAX_VALUE;
+
 public class Population {
     ArrayList<Schedule> schedule = new ArrayList<>();
-    
+    private int BestFT ;     //best finish time in this population
+
+    public int getBestFT() {
+        int minFT = MAX_VALUE ;      // best finish time
+        for (int i = 0; i < schedule.size(); i++) {
+            if(schedule.get(i).getsFT() < minFT){
+                minFT = schedule.get(i).getsFT();
+            }
+        }
+        return BestFT = minFT;
+    }
+
 }
